70deb6b8686b66e9ebae966a4b36f5a02fe1751f
Lara Helmli <lara.helmli@gmx.net>
2026-03-13 16:11:10 +0100

    LandingPage subtitle + heroimage

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,36 +1,40 @@
-import { AboutMePage } from "./AboutMePage";
-import { ProjectsPage } from "./ProjectsPage";
+import { AboutMePage } from "../sections/AboutMeSection";
+import { CollaborationSection } from "../sections/CollaborationSection";
+import { ProjectsPage } from "../sections/ProjectsSection";
 
 export function LandingPage() {
   return (
     <>
+    
       <section className="hero">
-        <img src="/ich3.png" alt="Lara Helmli" className="hero-image" />
-
-        <div className="hero-content">
-          <h1>
-            Hi, ich bin <span>Lara</span>
-          </h1>
-
-          <p className="hero-subtitle">
-            Medieninformatik Studentin an der BHT Berlin mit Fokus auf
-            Frontend-Entwicklung und modernen Webanwendungen.
-          </p>
-
-          <div className="hero-actions">
-            <a href="#projects" className="btn btn-primary">
-              Projekte ansehen
-            </a>
-            <a href="#about" className="btn btn-secondary">
-              Über mich
-            </a>
-          </div>
-        </div>
+      <div className="hero-content">
+  <div className="hero-main">
+    <h1>Lara</h1>
+    <img src="/ich5.jpg" alt="Lara Helmli" className="hero-image" />
+    <h1>Helmli</h1>
+    <p className="hero-subtitle">
+    Medieninformatik Studentin an der BHT Berlin mit Fokus auf
+    Frontend-Entwicklung und modernen Webanwendungen.
+  </p>
+    <div className="hero-actions">
+    <a href="#projects" className="btn btn-primary">
+      Projekte ansehen
+    </a>
+    <a href="#about" className="btn btn-secondary">
+      Kontakt
+    </a>
+  </div>
+  </div>
+</div>
       </section>
-
       <div className="page">
+        <hr/>
         <AboutMePage/>
+        <hr/>
         <ProjectsPage />
+        <hr/>
+        <CollaborationSection/>
+        <hr/>
       </div>
     </>
   );
