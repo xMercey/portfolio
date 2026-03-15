d08a2500ac4f6bc6ca5c13b4d8eed83da8dd8c03
Lara Helmli <lara.helmli@gmx.net>
2026-03-15 08:54:30 +0100

    Header in app.tsx hinzugefügt

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { Footer } from './components/Footer'
 import { LandingPage } from './pages/LandingPage'
 import { FinecashPage } from './projects/FinecashPage'
+import { Header } from './components/Header'
 
 
 function App() {
@@ -9,15 +10,13 @@ function App() {
     <BrowserRouter>
       <div className="app-shell">
  
-
+        <Header/>
         <main className="app-main">
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/finecash" element={<FinecashPage />} />
-
           </Routes>
         </main>
-
         <Footer />
       </div>
     </BrowserRouter>
