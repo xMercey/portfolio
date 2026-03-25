a5d58609fd450c164ebbc361a75fe0465245944d
Lara Helmli <lara.helmli@gmx.net>
2026-03-25 17:32:25 +0100

    Icons hinzugefügt

--- src/sections/AboutMeSection.tsx
+++ src/sections/AboutMeSection.tsx
@@ -1,16 +1,21 @@
+import { Palette, LayoutTemplate, Layers3 } from "lucide-react";
+
 export function AboutMePage() {
   const cards = [
     {
+      icon: Palette,
       title: "Design",
-      text: "Ausbildung als Gestaltungstechnischer Assistent, jetzt Medieninformatik-Studentin. Ich kombiniere Designverständnis mit technischer Umsetzung."
+      text: "Ich habe eine Ausbildung als Gestaltungstechnische Assistentin gemacht und studiere jetzt Medieninformatik. Dadurch verbinde ich Design mit technischer Umsetzung."
     },
     {
+      icon: LayoutTemplate,
       title: "Frontend",
-      text: "Ich interessiere mich für die Schnittstelle zwischen Design und Entwicklung. Also Interfaces gestalten und die Technik dahinter umsetzen."
+      text: "Mich interessiert vor allem die Verbindung von Design und Entwicklung. Ich gestalte Interfaces und setze sie im Code um."
     },
     {
+      icon: Layers3,
       title: "Full Stack",
-      text: "Neben Frontend interessiert mich auch Full Stack Entwicklung, um Anwendungen ganzheitlich zu verstehen, vom UI bis zur Backend-Logik."
+      text: "Neben Frontend interessiert mich auch Full Stack Entwicklung, um besser zu verstehen, wie Anwendungen insgesamt funktionieren, vom UI bis zum Backend."
     }
   ];
 
@@ -22,6 +27,7 @@ export function AboutMePage() {
         <div className="card-container">
           {cards.map((card) => (
             <div key={card.title} className="card">
+              <card.icon className="card-icon"/>
               <h3>{card.title}</h3>
               <p>{card.text}</p>
             </div>
